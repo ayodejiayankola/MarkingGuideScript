d655cfbc5dc9da92c885c476cb54fb64576328a2
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2020-04-27 09:23:46 +0100

    created all parameters and methods for the student paper class

--- classes/MarkingGuide.php
+++ classes/MarkingGuide.php
@@ -2,20 +2,21 @@
 
 
 class MarkingGuide{
-    //Parameters
-    public $subject;
-    public $question_no;
-    public $question;
-    public $correct_answer;
+    //PARAMETERS
+    protected $id;
+    protected $subject;
+    protected $question_no;
+    protected $correct_answer;
 
 
-    public function __construct($subject,$question_no,$question,$correct_answer)
+    public function __construct($id,$subject,$question_no,$correct_answer)
     {
 
     }
 
+    //METHODS
     //Create a new marking guide
-    public function createMarkingGuide(){
+    public function createMarkingGuide($id,$question_no,$correct_answer){
 
     }
 
@@ -25,7 +26,7 @@ class MarkingGuide{
     }
 
     //Remove/Delete a marking guide
-    public function deleteMarkingGuide(){
+    public function deleteMarkingGuide($id){
 
     }
 
@@ -34,7 +35,7 @@ class MarkingGuide{
 
     }
 
-    //Quit(End the loop )
+    //Quit(End the loop)
     public function quit(){
 
     }
@@ -45,4 +46,28 @@ class MarkingGuide{
 
 class StudentPaper extends MarkingGuide {
 
+    //PARAMETERS
+    private $choice_answer;
+
+    public function __construct($choice_answer,$id,$subject,$question_no,$correct_answer)
+    {
+
+
+        parent::__construct($id,$subject,$question_no,$correct_answer);
+
+
+    }
+
+    //METHODS
+    // Submit Student Paper
+    public function submitStudentAnswer($id,$question_no,$choice_answer){
+
+
+    }
+
+    //Mark Student Paper
+    public function markStudentPaper($choice_answer,$correct_answer){
+
+    }
 }
+
