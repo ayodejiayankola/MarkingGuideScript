48c7ed02ea30c83a4d11d88a6844cd6f4b244303
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2020-04-29 02:00:10 +0100

    noted some comments

--- classes/Paper.php
+++ classes/Paper.php
@@ -1,15 +1,16 @@
 <?php
 
 
-class MarkingGuide{
+class Paper{
     //PARAMETERS
     protected $id;
     protected $subject;
-    protected $question_no;
-    protected $correct_answer;
+    protected $question_no =[];
+    protected $correct_answer=[];
+    protected $choice_answer=[];
 
 
-    public function __construct($id,$subject,$question_no,$correct_answer)
+    public function __construct($id,$subject,$question_no,$correct_answer,$choice_answer)
     {
 
     }
@@ -35,32 +36,8 @@ class MarkingGuide{
 
     }
 
-    //Quit(End the loop)
-    public function quit(){
-
-    }
-
-
-}
-
-
-class StudentPaper extends MarkingGuide {
-
-    //PARAMETERS
-    private $choice_answer;
-
-    public function __construct($choice_answer,$id,$subject,$question_no,$correct_answer)
-    {
-
-
-        parent::__construct($id,$subject,$question_no,$correct_answer);
-
-
-    }
-
-    //METHODS
     // Submit Student Paper
-    public function submitStudentAnswer($id,$question_no,$choice_answer){
+    public function studentPaper($id,$subject,$question_no,$choice_answer){
 
 
     }
@@ -69,4 +46,41 @@ class StudentPaper extends MarkingGuide {
     public function markStudentPaper($choice_answer,$correct_answer){
 
     }
+
+    //Quit(End the loop)
+    public function quit(){
+
+    }
+
+
 }
+
+class MarkingGuide{
+    private $paper;
+    public function __construct(Paper $paper)
+    {
+        $this->paper=$paper;
+    }
+
+    public function createMarkingGuide(){
+        $this->paper->newMarkingGuide();
+    }
+}
+class StudentPaper{
+    private $paper;
+    public function __construct(Paper $paper)
+    {
+        $this->paper=$paper;
+    }
+
+    public function createSubmission(){
+        $this->paper->studentPaper();
+    }
+}
+
+
+
+//created a new instance of the object
+$paper= new Paper();
+$MarkingGuide =new MarkingGuide($paper);
+$MarkingGuide->createMarkingGuide(); //create a new marking guide
